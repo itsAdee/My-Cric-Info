2239080963569b5a51498ea8780a05061122f8a4
itsAdee <adeelahmedqureshi02@gmail.com>
2022-12-23 05:30:32 +0500

    Added minimum database

--- MysqlConnection/Insertion.py
+++ MysqlConnection/Insertion.py
@@ -9,6 +9,7 @@ myconnection = pymysql.connect(
 myconnection.autocommit(True)
 mycursor = myconnection.cursor()
 cwd = Path.cwd().parent
+convert_match = True
 
 
 def InsertPlayers():
@@ -76,7 +77,12 @@ def InsertTeams():
 
 def InsertMatches():
     cwd = Path.cwd().parent
-    file = open(cwd / 'Filteration//Match.csv', 'r')
+    my_teams = pandas.read_csv(cwd / 'Filteration//Teams.csv')
+    if convert_match:
+        match_data = pandas.read_csv(
+            cwd / 'Filteration//Match.csv', parse_dates=['MatchDate'])
+        match_data.to_csv('Match.csv', index=False)
+    file = open('Match.csv', 'r')
     mycursor.execute('USE cricinfosystem2')
     csv_data = csv.reader(file)
     skipHeader = True
@@ -89,40 +95,62 @@ def InsertMatches():
         match_id = rows[0]
         series_id = rows[1]
         match_type = rows[2]
-        match_date = rows[3]
+        match_date = rows[3].split(' ')[0]
         event_name = rows[4]
         event_stage = rows[5]
         field_umpire_1 = rows[6]
         field_umpire_2 = rows[7]
         Tv_Umpire = rows[8]
         winner_team = rows[9]
+        winner_team = my_teams.loc[my_teams['TeamName'] == winner_team]
+        winner_team = winner_team['Team_id'].iloc[0]
         result_description = rows[10]
         player_of_match = rows[11]
-        team_1 = rows[13]
-        teams_2 = rows[14]
-        q = 'Insert into matchfixture( Match_id,series_id,match_type,MatchDate,event_name,event_stage,field_umpire_1,field_umpire_2,TV_empire,fk_team_1_id,fk_team_2_id,Matchwinner,Matchwinner_desc,player_of_match) VALUES (%s,%s,%s,%s)'
+        team_1 = rows[14]
+        print(team_1)
+        team_id = my_teams.loc[my_teams['TeamName'] == team_1]
+        print(team_1)
+        team_id = team_id['Team_id'].iloc[0]
+        print(team_1)
+        teams_2 = rows[15]
+        teams_2id = my_teams.loc[my_teams['TeamName']
+                                 == teams_2]
+        teams_2id = teams_2id['Team_id'].iloc[0]
+        q = 'Insert into matchfixture( Match_id,series_id,match_type,MatchDate,event_name,event_stage,field_umpire_1,field_umpire_2,TV_empire,fk_team_1_id,fk_team_2_id,Matchwinner,Matchwinner_desc,player_of_match) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
         values = (match_id, series_id, match_type, match_date, event_name, event_stage, field_umpire_1,
-                  field_umpire_2, Tv_Umpire, winner_team, result_description, player_of_match, team_1, teams_2)
+                  field_umpire_2, Tv_Umpire, team_id, teams_2id, winner_team, result_description, player_of_match)
         mycursor.execute(q, values)
 
 
 def Insertballs():
     cwd = Path.cwd().parent
     file = open(cwd / 'Filteration//BallDatabase.csv', 'r')
+    valid_combinations = []
     mycursor.execute('USE cricinfosystem2')
     csv_data = csv.reader(file)
     skipHeader = True
     mycursor.execute('delete from balls')
-    for rows in file:
+    for rows in csv_data:
+        if skipHeader:
+            skipHeader = False
+            continue
         Match_id = rows[0]
+        print(Match_id)
         Inning = rows[1]
+        print(Inning)
         overNumber = rows[2]
         BallNumber = rows[3]
         extra_runs = rows[4]
         scored_runs = rows[5]
         bowler = rows[6]
+        if (Match_id, Inning, overNumber, BallNumber) in valid_combinations:
+            continue
+        valid_combinations.append((Match_id, Inning, overNumber, BallNumber))
+
+        print(bowler)
         batter = rows[7]
-        q = 'Insert into balls(Match_id,inning,OverNumber,BallNumber,extra_runs,scored_runs,bowler,batter)'
+        print(batter)
+        q = 'Insert into balls(Match_id,inning,OverNumber,BallNumber,extra_runs,scored_runs,bowler,batter) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
         values = (Match_id, Inning, overNumber, BallNumber,
                   extra_runs, scored_runs, bowler, batter)
         mycursor.execute(q, values)
@@ -132,16 +160,24 @@ def InsertBatter():
     cwd = Path.cwd().parent
     file = open(cwd / 'Filteration//BatterInfo.csv', 'r')
     mycursor.execute('USE cricinfosystem2')
+    valid_combinations = []
     csv_data = csv.reader(file)
     skipHeader = True
     mycursor.execute('delete from batterstatsininning')
-    for rows in file:
+    for rows in csv_data:
+        if skipHeader:
+            skipHeader = False
+            continue
         Match_id = rows[0]
         Inning = rows[1]
         batter = rows[2]
+        if (Match_id, Inning, batter) in valid_combinations:
+            continue
+        valid_combinations.append((Match_id, Inning, batter))
+
         runs = rows[3]
         balls = rows[4]
-        q = 'Insert into batterstatsininning(fk_Match_id,InningNumber,fk_batter_id,runs_scored,balls_faced)'
+        q = 'Insert into batterstatsininning(fk_Match_id,InningNumber,fk_batter_id,runs_scored,ballfaced) values (%s,%s,%s,%s,%s)'
         values = (Match_id, Inning, batter, runs, balls)
         mycursor.execute(q, values)
 
@@ -150,17 +186,25 @@ def InsertBowler():
     cwd = Path.cwd().parent
     file = open(cwd / 'Filteration//BowlerInfo.csv', 'r')
     mycursor.execute('USE cricinfosystem2')
+    valid_combinations = []
     csv_data = csv.reader(file)
     skipHeader = True
     mycursor.execute('delete from bowlerstatsininning')
-    for rows in file:
+    for rows in csv_data:
+        if skipHeader:
+            skipHeader = False
+            continue
         bowler = rows[0]
         runs = rows[1]
         deliveries = rows[2]
         match_id = rows[3]
         innings = rows[4]
-        q = 'Insert into batterstatsininning(fk_Match_id,InningNumber,fk_bowler_id,runs,deliveries)'
-        values = (bowler, runs, deliveries, match_id, innings)
+        if (match_id, innings, bowler) in valid_combinations:
+            continue
+        valid_combinations.append((match_id, innings, bowler))
+        print(match_id)
+        q = 'Insert into bowlerstatsininning(fk_Match_id,InningNumber,fk_bowler_id,runs,deliveries) values (%s,%s,%s,%s,%s)'
+        values = (match_id, innings, bowler, runs, deliveries)
         mycursor.execute(q, values)
 
 
@@ -168,10 +212,14 @@ def InsertWicketsInfo():
     cwd = Path.cwd().parent
     file = open(cwd / 'Filteration//wicketinfo.csv', 'r')
     mycursor.execute('USE cricinfosystem2')
+    valid_combinations = []
     csv_data = csv.reader(file)
     skipHeader = True
     mycursor.execute('delete from wicketstatsinnings')
-    for rows in file:
+    for rows in csv_data:
+        if skipHeader:
+            skipHeader = False
+            continue
         batter = rows[0]
         bowler = rows[1]
         Inning = rows[3]
@@ -179,6 +227,12 @@ def InsertWicketsInfo():
         over = rows[8]
         Ball = rows[9]
         Description = rows[10]
-        q = 'Insert into wicketstatsinnings(BatterId,BowlerId,InningNumber,fk_Match_id,OverNumber,BallNumber,WicketDescription)'
+        if (batter, bowler, Inning, match_id) in valid_combinations:
+            continue
+        valid_combinations.append((batter, bowler, Inning, match_id))
+        q = 'Insert into wicketstatsinnings(BatterId,BowlerId,InningNumber,fk_Match_id,OverNumber,BallNumber,WicketDescription) values (%s,%s,%s,%s,%s,%s,%s)'
         values = (batter, bowler, Inning, match_id, over, Ball, Description)
         mycursor.execute(q, values)
+
+
+InsertWicketsInfo()
