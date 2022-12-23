01fdd4ce707359a7a98020496f56b30922788582
AreebAhmad-02 <chaudhry.bese21seecs@seecs.edu.pk>
2022-12-23 18:20:35 +0500

    player, score card, home, team page added

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
 
 
-@app.route('/')
+class MyForm(FlaskForm):
+    team_name = StringField('team_name', validators=[DataRequired()])
+    season = StringField('season', validators=[DataRequired()])
+    match_type = StringField('match_type', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/index')
 def index():
-    live = [{'India': [{'rohit': [140, 113], 'kohli':[77, 65],
-                        'dhoni':[57, 78], 'jadeja':[0, 1], 'raina':[26, 18], 'pandya':[35, 16]}, {
-        'amir': [3, 47, 10],
-        'shadab':[1, 71, 10], 'shoaib':[1, 84, 9], 'hassan':[1, 104, 20]
-    }], 'score':[336, 5], 'overs':50}]
-    key = list(live[0].keys())
-    return render_template('Home_page.html', team=key, live_s=live)
+
+    return render_template('index.html')
 
 
 @app.route('/home')
@@ -27,6 +37,38 @@ def stats():
 @app.route('/sample')
 def sample():
     return render_template('sample.html')
+
+
+@app.route('/player')
+def player():
+    return render_template('player.html')
+
+
+@app.route('/teams', methods=['GET', 'POST'])
+def teams():
+    page = 'live matches'
+    messages = []
+    team_name = None
+    form = MyForm()
+    if form.validate_on_submit():
+        team_name = form.team_name.data
+        form.team_name.data = ''
+        
+    if form.validate_on_submit():
+        team_name = form.season.data
+        form.season.data = ''
+        
+    if form.validate_on_submit():
+        team_name = form.match_type.data
+        form.match_type.data = ''
+        
+    if request.method == 'POST':
+        team_name = request.form["team_name"]
+        team_name = request.form["season"]
+        team_name = request.form["match_type"]
+        return render_template('Home_page.html', form=form, page=page, name=team_name)
+
+    return render_template('teams.html', form=form, name=team_name)
 
 
 if __name__ == '__main__':
