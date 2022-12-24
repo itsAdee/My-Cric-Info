e737713a30fc1b2f017784e48c8389bb502152da
itsAdee <adeelahmedqureshi02@gmail.com>
2022-12-24 20:11:38 +0500

    Connection

---
+++ MysqlConnection/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+batter_id = batter[0]
+        # mycursor.execute(
+        #     'select concat(first_name, " ", last_name) from player where player_id = %s', [batter_id])
+        # print(mycursor.fetchall())
+        # # batter[0] = mycursor.fetchall()[0]
+        # # print(batter)
