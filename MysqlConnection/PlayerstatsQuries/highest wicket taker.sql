1fc7d8604d0bc62cd76d5915bd762d15a1e5cf93
itsAdee <adeelahmedqureshi02@gmail.com>
2022-12-26 12:04:54 +0500

    Added all queries

--- MysqlConnection/PlayerstatsQuries/highest wicket taker.sql
+++ MysqlConnection/PlayerstatsQuries/highest wicket taker.sql
@@ -1,4 +1,5 @@
-select sum(runs_scored) as total_runs , fk_batter_id from batterstatsininning
-where fk_batter_id in ( select player_id  from player where Country = "Pakistan")
+-- this query is a part of python script
+(select count(*)  as wickets , BowlerId from wicketstatsinnings
+where BowlerId in ( select player_id  from player where Country = "Pakistan")
 and fk_Match_Id in (select Match_id from matchfixture where match_type = 'T20')
-group by fk_batter_id
+group by BowlerId) 
---
+++ MysqlConnection/TeamstatsQueries/No of losses.sql
@@ -0,0 +1,6 @@
+-- No of losses
+select count(*) as losses from matchfixture
+where (fk_team_1_id in ( select team_id from team  where  team_name = 'Pakistan')
+or fk_team_2_id in (select team_id from team where team_name = 'Pakistan')) and
+Matchwinner not  in ( select team_id from team where team_name = 'Pakistan') and Matchwinner != 0
+and match_type = 'T20'
